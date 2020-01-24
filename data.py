7b4b41f9ea7b17e111eb2aa4b16ed65cd20a68a1
Tiago Villa <tiago_villa@hotmail.com>
2020-01-23 23:14:26 -0300

    Rotas e layouts das páginas

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Artigo Um',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde illo impedit,perspiciatis amet esse tenetur odio neque hic? Commodi, nulla.',
+            'author': 'Tiago Villa',
+            'create_date': '20-01-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Artigo Dois',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde illo impedit,perspiciatis amet esse tenetur odio neque hic? Commodi, nulla.',
+            'author': 'Tiago Villa',
+            'create_date': '20-01-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Artigo Três',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde illo impedit,perspiciatis amet esse tenetur odio neque hic? Commodi, nulla.',
+            'author': 'Tiago Villa',
+            'create_date': '20-01-2020'
+        }
+    ]
+
+    return articles
